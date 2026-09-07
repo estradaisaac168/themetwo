83a5fa26312b01d854c4603eda2cf87c2fc3d51b
Isaac Estrada <isaacestrada168@outlook.com>
2026-09-07 00:07:01 -0600

    Restructuracion menu

--- global-templates/navbar.php
+++ global-templates/navbar.php
@@ -30,21 +30,46 @@
         </div>
 
         <div id="content-menu" class="content-menu">
-            <?php
+            <div class="container">
+                <div class="content-menu-header">
 
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'menu-1',
-                    'menu_id'        => 'primary-menu',
-                    'menu_class'     => 'ul-menu',
-                    'container'      => false,
-                )
-            );
-            ?>
+                    <div class="site-branding">
+                        <?php
+                        the_custom_logo();
+                        if (is_front_page() && is_home()) :
+                        ?>
+                            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+                        <?php
+                        else :
+                        ?>
+                            <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+                        <?php
+                        endif;
+                        $themetwo_description = get_bloginfo('description', 'display');
+                        if ($themetwo_description || is_customize_preview()) :
+                        ?>
+                            <p class="site-description"><?php echo $themetwo_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+                                                        ?></p>
+                        <?php endif; ?>
+                    </div><!-- .site-branding -->
 
-            <span id="button-close" class="button-close">
-                <i class="close-icon bi bi-x-lg text-white"></i>
-            </span>
+                    <span id="button-close" class="button-close">
+                        <i class="close-icon bi bi-x-lg text-white"></i>
+                    </span>
+                </div>
+
+                <?php
+
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                        'menu_class'     => 'ul-menu',
+                        'container'      => false,
+                    )
+                );
+                ?>
+            </div>
         </div>
 
     </div>
